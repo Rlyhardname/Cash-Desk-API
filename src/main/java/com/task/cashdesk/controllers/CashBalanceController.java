e61777b7e2276a24164d33d3b14eb51050dacab9
Ivan Dimitrov <mmo_unstable@abv.bg>
2024-06-16 18:14:19 +0300

    -add getBalance endpoint, returning CashDeskDTO with information for CurrentCash reserves, available currencies and Cashier or Validation message.

--- src/main/java/com/task/cashdesk/controllers/CashBalanceController.java
+++ src/main/java/com/task/cashdesk/controllers/CashBalanceController.java
@@ -1,12 +1,39 @@
 package com.task.cashdesk.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.task.cashdesk.exceptions.DailyDepositAndWithDrawRequirementsNotMetException;
+import com.task.cashdesk.models.entity.Cash;
+import com.task.cashdesk.services.BalanceService;
+import com.task.cashdesk.utils.ApiKeyValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
+@RestController
 @RequestMapping("/api/v1/cash-balance")
 public class CashBalanceController {
-    @GetMapping("")
-    void loadIndex(){
+
+    private final BalanceService balanceService;
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    public CashBalanceController(BalanceService balanceService) {
+        this.balanceService = balanceService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<?> getBalance(@RequestHeader() String apiKey) {
+        if (!ApiKeyValidator.isValid(apiKey)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            return new ResponseEntity<>(balanceService.getBalance(), HttpStatus.OK);
+        } catch (DailyDepositAndWithDrawRequirementsNotMetException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
 
     }
 
